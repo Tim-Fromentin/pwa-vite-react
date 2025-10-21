9b5a76b90cce5a173bdc2d38b1d38320f972c3dc
Tim <tim.fromentin.dev.pro@gmail.com>
2025-10-21 16:25:01 +0200

    feat(vite.config.js): add manifest for the pwa

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -1,11 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: 'PWA Router',
+        short_name: 'PWA Router',
+        description: 'Mon application PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: './index.html',
+        icons: [
+          {
+            src: 't.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 't.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
-
-
-
-
